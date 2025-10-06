fe77b9f03c5d905cbaf79f6e73246783117570c8
JuanCarlos <melozajuancarlos@gmail.com>
2025-10-06 18:19:02 +0200

    feat: agregar lógica de enrutamiento y estructura inicial en main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-import './styles/style.css'
+import "./styles/style.css";
+import { router } from './router.js'
+
+document.querySelector("#app").innerHTML = `
+    
+    <main id="main-content" class="p-6"></main>
+    `;
+// Navegar al inicio al hacer clic en el título
+document.getElementById('home-link').addEventListener('click', () => {
+  history.pushState({}, '', '/')
+  router()
+})
+
+// Cargar la ruta actual
+router()
+
+// Detectar navegación del historial
+window.addEventListener('popstate', router)
