e61ba5cbd562975ddfb260615518f28101fe272f
JCMeloza <melozajuancarlos@gmail.com>
2025-10-10 12:48:50 +0200

    añadido ruta favoritos

--- src/router.js
+++ src/router.js
@@ -1,16 +1,19 @@
 import { HomePage } from "./pages/HomePage.js";
 import { CharacterDetailPage } from "./pages/CharacterDetailPage.js";
-
+import { FavoritesPage } from "./pages/FavoritesPage.js";
 export function router() {
-	const path = window.location.pathname;
-	const main = document.getElementById("main-content");
+    const path = window.location.pathname;
+    const main = document.getElementById("main-content");
 
-	if (path === "/" || path === "/index.html") {
-		HomePage();
-	} else if (path.startsWith("/character/")) {
-		const id = path.split("/character/")[1];
-		CharacterDetailPage(id);
-	} else {
-		main.innerHTML = `<p class="text-center text-gray-400">Página no encontrada 😢</p>`;
-	}
+    if (path === "/" || path === "/index.html") {
+        HomePage();
+    } else if (path.startsWith("/character/")) {
+        const id = path.split("/character/")[1];
+        CharacterDetailPage(id);
+    } else if (path === "/favorites") {
+       
+        FavoritesPage();
+    } else {
+        main.innerHTML = `<p class="text-center text-gray-400">Página no encontrada 😢</p>`;
+    }
 }
