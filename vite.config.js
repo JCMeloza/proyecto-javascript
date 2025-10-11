b74d753ce1ad241cd3f5ef6f8528b3703fcda251
JCMeloza <melozajuancarlos@gmail.com>
2025-10-11 09:01:37 +0200

    configuración para gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	base: '/proyecto-javascript/',
 	server: {
 		watch: {
 			usePolling: true,
